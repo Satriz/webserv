2544f2f82b935720fe2089b680c6a4d05f532641
Satriz <48471649+Satriz@users.noreply.github.com>
2019-03-12 16:08:29 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,20 +1,12 @@
-<?php
-    echo"<h1>Hello World</h1> <br/>";
-    
-    $text = "<table>
-    <tr>
-        <td>NIM</td>
-        <td> : </td>
-        <td>16.01.53.0064</td>
-    </tr>
-    <tr>
-        <td>Nama</td>
-        <td> : </td>
-        <td>Dimas Ahmad Rizal</td>
-    </tr>
-</table> <br>";
+<!DOCTYPE html>
+<html>
+<body>
 
-    echo $text;
+<?php
+echo "<h1>Hello World!</h1>";
+echo "Nama	: Satria Dista Putra Faudi<br>";
+echo "NIM	: 16.01.53.0168";
 ?>
 
-    <a href="/akademik">Go To Akademik</a>
+</body>
+</html>
